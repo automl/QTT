5eb99036e5e9620e6013e85490d13e32c52018c6
rapanti <ivo.rapant@uranus.uni-freiburg.de>
2024-08-08 12:47:46 +0200

    update 08/08/24/1

--- examples/quicktuning.py
+++ examples/quicktuning.py
@@ -1,8 +1,15 @@
-from qtt.factory import get_optimizer
-from qtt.tuners import QuickTuner
-from qtt.finetune.cv.classification import finetune_script
+from qtt import QuickTuner, QuickOptimizer
+from qtt.finetune.cv.classification import finetune_script, extract_task_info_metafeat
 
-optimizer = get_optimizer("mtlbm/micro")
-qt = QuickTuner(optimizer, finetune_script)
-task_info = {"data_path": "path/to/data"}
-qt.run(task_info=task_info, time_budget=3600)
+# load a pretrained optimizer
+optimizer = QuickOptimizer.load("path/to/optimizer")
+
+# setup the optimizer
+n = 100  # number of configs to sample
+# we need to extract some information about the dataset
+task_info, metafeat = extract_task_info_metafeat("path/to/dataset")
+optimizer.setup(n, metafeat)
+
+# setup and run the tuner
+qt = QuickTuner(optimizer, finetune_script)  # pass the optimizer and objective function
+qt.run(task_info=task_info, time_budget=3600)  # run for 1 hour
